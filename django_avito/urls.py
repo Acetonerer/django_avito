27da6168ce610ae76fc99abe5a8e965348c7f7e9
Acetonerer <bolokan2014@gmail.com>
2024-04-05 15:38:06 +0700

    change

--- django_avito/urls.py
+++ django_avito/urls.py
@@ -18,7 +18,7 @@ from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from django.contrib import admin
 from django.urls import path, include
-from account.views import MainView
+
 schema_view = get_schema_view(
     openapi.Info(
         title="API Documentation",
@@ -33,7 +33,6 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-    path('', MainView.as_view(), name='main'),
     path('admin/', admin.site.urls),
     path('', include('account.urls')),
     path('', include('users.urls')),
