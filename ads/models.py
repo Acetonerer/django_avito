81ee27411543e06ffa992f2675317cb00ad1ef86
Acetonerer <bolokan2014@gmail.com>
2024-04-12 12:47:20 +0700

    changes at 12.04.2024

--- ads/models.py
+++ ads/models.py
@@ -3,7 +3,7 @@ from account.models import Account
 
 
 class Ad(models.Model):
-    id = models.IntegerField(default=0, unique=True)
+    id = models.IntegerField(default=0, unique=True, primary_key=True)
     title = models.CharField(max_length=100)
     status = models.CharField(max_length=20, default='active')
     account_id = models.ForeignKey(Account, on_delete=models.CASCADE)
