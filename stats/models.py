1105e9d6d942dc33ed14bf5695e18baa525c1e21
Acetonerer <bolokan2014@gmail.com>
2024-04-19 16:30:18 +0700

    changes at 19.04.2024

--- stats/models.py
+++ stats/models.py
@@ -7,7 +7,7 @@ from ads.models import Ad
 class Statistics(models.Model):
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
     date = models.DateField()
-    ad = models.ForeignKey(Ad, on_delete=models.CASCADE)
+    ad = models.ForeignKey(Ad, on_delete=models.CASCADE, default=0)
     uniq_contacts = models.IntegerField(default=0)
     uniq_favorites = models.IntegerField(default=0)
     uniq_views = models.IntegerField(default=0)
