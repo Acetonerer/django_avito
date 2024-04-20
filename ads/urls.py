da95194912642993607614fb36ad1c484caa45b8
Acetonerer <bolokan2014@gmail.com>
2024-04-20 13:54:41 +0700

    тестовый фикс сервера

--- ads/urls.py
+++ ads/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AdListView
 
 urlpatterns = [
-    path('ads/<user_id>/<account_id>/', AdListView.as_view(), name='ads-list'),
+    path('deps/<user_id>/<account_id>/', AdListView.as_view(), name='ads-list'),
 ]
--- stats/urls.py
+++ stats/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import StatisticsView
 
 urlpatterns = [
-    path('stats/<user_id>/<account_id>/items', StatisticsView.as_view(), name='stats'),
+    path('stats/<user_id>/<account_id>/', StatisticsView.as_view(), name='stats'), #post
     path('stats/statistics', StatisticsView.as_view(), name='statistics'),
 ]
