0c9b8319a81e0e35dd672c94bd4ff0d8b0f91edd
Acetonerer <bolokan2014@gmail.com>
2024-04-20 21:44:10 +0700

    тестовый фикс сервера

--- Dockerfile
+++ Dockerfile
@@ -2,14 +2,10 @@ FROM python:3.11
 
 WORKDIR /code
 
-RUN pip install --upgrade pip
-RUN pip install celery
-RUN pip install redis
 COPY ./requirements.txt /code/
+
 RUN pip install -r /code/requirements.txt
 
 COPY . .
-
-ENV DJANGO_SETTINGS_MODULE=django_avito.settings
 
 CMD ["gunicorn", "django_avito.wsgi:application", "--bind", "0.0.0.0:8000"]
--- django_avito/celery.py
+++ django_avito/celery.py
@@ -1,17 +1,16 @@
-from __future__ import absolute_import
+from __future__ import absolute_import, unicode_literals
 import os
 from celery import Celery
-from django.conf import settings
 
-# Установка переменной окружения для настроек Django
+
+# Установка переменной окружения для настроек проекта
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'django_avito.settings')
 
+# Создание экземпляра объекта Celery
 app = Celery('django_avito')
-app.conf.update(
-    CELERY_BROKER_URL='redis://redis:6379/0',
-    CELERY_ACCEPT_CONTENT=['json'],
-    CELERY_RESULT_BACKEND='redis://redis:6379/0',
-    CELERY_TIMEZONE='Europe/Moscow',
-)
-app.config_from_object(settings, namespace='CELERY')
-app.autodiscover_tasks(settings.INSTALLED_APPS, related_name='tasks')
+
+# Загрузка настроек из файла Django
+app.config_from_object('django.conf:settings', namespace='CELERY')
+
+# Автоматическое обнаружение и регистрация задач из файлов tasks.py в приложениях Django
+app.autodiscover_tasks()
