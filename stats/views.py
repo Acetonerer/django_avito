08f5e83c8e2d44a0f41e317baa16f3673991d35b
Acetonerer <bolokan2014@gmail.com>
2024-04-23 18:30:24 +0700

    Вывод статистики из бд + данные за сегодня

--- stats/views.py
+++ stats/views.py
@@ -6,6 +6,8 @@ import requests
 from account.models import Account
 from rest_framework.response import Response
 from django.utils import timezone
+
+from account.token import get_new_access_token
 from ads.models import Ad
 from stats.models import Statistics
 
@@ -102,15 +104,16 @@ class StatisticsView(APIView):
 
     def get(self, request):
         try:
-            account_user_id = int(request.query_params.get('account_id'))
+            account_id = int(request.query_params.get('account_id'))
             start_date_str = request.query_params.get('start_date')
             end_date_str = request.query_params.get('end_date')
 
             start_date = datetime.strptime(start_date_str, '%Y-%m-%d').date()
             end_date = datetime.strptime(end_date_str, '%Y-%m-%d').date()
 
+            # Получаем статистику из базы данных
             statistics = Statistics.objects.filter(
-                account_id=account_user_id,
+                account_id=account_id,
                 date__range=(start_date, end_date)
             )
 
@@ -131,9 +134,103 @@ class StatisticsView(APIView):
                     'uniqViews': stat.uniq_views
                 })
 
+            # Получаем статистику от Avito за сегодняшний день
+            today = timezone.now().date()
+            account = get_object_or_404(Account, account_id=account_id)
+            access_token = account.access_token
+            client_id = account.client_id
+            client_secret = account.client_secret
+
+            ads = Ad.objects.filter(account_id=account_id)
+            item_ids = [ad.ad_id for ad in ads]
+
+            date_from = today
+            date_to = today
+
+            date_from_str = date_from.isoformat()
+            date_to_str = date_to.isoformat()
+
+            fields = ["uniqViews", "uniqContacts", "uniqFavorites"]
+            period_grouping = "day"
+
+            data = {
+                "dateFrom": date_from_str,
+                "dateTo": date_to_str,
+                "fields": fields,
+                "itemIds": item_ids,
+                "periodGrouping": period_grouping
+            }
+
+            url = f"https://api.avito.ru/stats/v1/accounts/{account.account_user_id}/items"
+            headers = {
+                "Authorization": f"Bearer {access_token}",
+                "Content-Type": "application/json"
+            }
+
+            response = requests.post(url, json=data, headers=headers)
+
+            if response.status_code == 403:
+                new_access_token, error = get_new_access_token(client_id, client_secret)
+
+                if error:
+                    return Response({'error': error}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+                # Обновление токена в объекте account
+                account.access_token = new_access_token
+                account.save()
+
+                url = f"https://api.avito.ru/stats/v1/accounts/{account.account_user_id}/items"
+                headers = {
+                    "Authorization": f"Bearer {access_token}",
+                    "Content-Type": "application/json"
+                }
+
+                response = requests.post(url, json=data, headers=headers)
+
+            elif response.status_code == 200:
+
+                statistics_data = response.json()
+
+                if 'result' in statistics_data and 'items' in statistics_data['result']:
+                    items = statistics_data['result']['items']
+
+                    today_stats = {
+                        'date': today.isoformat(),
+                        'ads': []
+                    }
+
+                    for item in items:
+                        item_id = item['itemId']
+                        stats_list = item['stats']
+
+                        if stats_list:
+                            stats_entry = stats_list[0]
+                            uniq_contacts = stats_entry.get('uniqContacts', 0)
+                            uniq_favorites = stats_entry.get('uniqFavorites', 0)
+                            uniq_views = stats_entry.get('uniqViews', 0)
+                        else:
+                            uniq_contacts, uniq_favorites, uniq_views = 0, 0, 0
+
+                        today_stats['ads'].append({
+                            'ad_id': item_id,
+                            'uniqContacts': uniq_contacts,
+                            'uniqFavorites': uniq_favorites,
+                            'uniqViews': uniq_views
+                        })
+
+                    response_data['statistics'].append(today_stats)
+                else:
+                    return Response({'error': 'Invalid response format from Avito'},
+                                    status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+            else:
+                return Response({'error': f"Failed to retrieve Avito statistics. Status code: {response.status_code}"},
+                                status=response.status_code)
+
             return Response(response_data)
 
         except ValueError as ve:
             return Response({'error': f"Invalid date format: {str(ve)}"}, status=status.HTTP_400_BAD_REQUEST)
+        except Account.DoesNotExist:
+            return Response({'error': 'Account not found'}, status=status.HTTP_404_NOT_FOUND)
         except Exception as e:
             return Response({'error': f"An error occurred: {str(e)}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
