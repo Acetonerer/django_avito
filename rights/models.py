978719f5675b2848942885bf39730b305f1e64c7
Acetonerer <bolokan2014@gmail.com>
2024-04-29 12:03:26 +0700

    Функционал добавления и изменения прав

--- rights/models.py
+++ rights/models.py
@@ -5,7 +5,8 @@ from users.models import User
 class UserRights(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     user_crm_id = models.CharField(max_length=100, unique=True)  # Изменено на уникальное поле
-    rights = models.CharField(max_length=20, choices=[('editing', 'Editing'), ('view', 'View'), ('noaccess', 'No Access')])
+    rights = models.CharField(max_length=20, choices=[('editing', 'Editing'), ('view', 'View'),
+                                                      ('noaccess', 'No Access')])
 
     def __str__(self):
         return f"{self.user_crm_id} - {self.rights}"
--- rights/urls.py
+++ rights/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import RightsView
 
 urlpatterns = {
-    path('rights/<user_id>/', RightsView.as_view()),  # GET, PUT
-    path('rights/add', RightsView.as_view()),  # POST
+    path('rights/<user_id>/', RightsView.as_view(), name='rights-detail'),  # GET, PUT
+    path('rights', RightsView.as_view(), name='rights-add'),  # POST
 }
