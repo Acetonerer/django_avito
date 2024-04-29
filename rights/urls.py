b6c8ad7c7c3e46e3270fe316d64a306d14893e66
Acetonerer <bolokan2014@gmail.com>
2024-04-29 11:34:47 +0700

    Функционал добавления и изменения прав

--- rights/urls.py
+++ rights/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from .views import RightsView
 
-urlpatterns = [
-    path('rights/<user_id>/', RightsView.as_view()),
-]
+urlpatterns = {
+    path('rights/<user_id>/', RightsView.as_view()),  # GET, PUT
+    path('rights/', RightsView.as_view()),  # POST
+}
